862d0228236baa60e2fe12758d3013f0e749f167
Anirudh Narayanan <anirudhn18@yahoo.com>
2016-08-06 03:47:10 -0500

    removing dummy files, creating final files

---
+++ codes/01-data-creation.R
@@ -0,0 +1,31 @@
+
+
+setwd("C:/Users/Anirudh Narayanan/Desktop/
+      Some stuff/msba stuff/Summer/Stats/msba-stats-project/codes")
+
+
+##WEATHER DATA PROCESSING
+dataloc = '../data/raw/weather/'
+
+weather_data = read.csv(paste0(dataloc,'msp-weather.txt'))
+weather_data = rbind(weather_data,read.csv(paste0(dataloc,'dfw-weather.txt')))
+weather_data$location = c(rep('MSP',396),rep('MSP',396))
+
+colnames(weather_data) = tolower(gsub('\\.',"_",colnames(weather_data)))
+write.csv(weather_data,file = '../data/final/final_weather_data.csv',
+          row.names = F)
+
+
+##AIRPORT PROCESSING
+dataloc = '../data/raw/delays/'
+filelist = list.files(dataloc)
+
+delay_data = data.frame()
+
+for (a.file in filelist){
+delay_data = rbind(delay_data,read.csv(paste0(dataloc,a.file)))
+}
+
+write.csv(delay_data[delay_data$ORIGIN %in% c('MSP','DFW'),],
+          file= '../data/final/final_delay_data.csv',
+          row.names = F)
