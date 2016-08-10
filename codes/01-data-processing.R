9dc71e5e3e22b0c4182d403cdcb6f79e9f608094
Anirudh Narayanan <anirudhn18@yahoo.com>
2016-08-10 01:31:48 -0500

    updated files

--- codes/01-data-processing.R
+++ codes/01-data-processing.R
@@ -6,8 +6,9 @@ dataloc = '../data/raw/weather/'
 weather_data = read.csv(paste0(dataloc,'msp-weather.txt'))
 weather_data = rbind(weather_data,read.csv(paste0(dataloc,'dfw-weather.txt')))
 weather_data$location = c(rep('MSP',396),rep('DFW',396))
-weather_data$FL_DATE = format(as.Date(as.character(weather_data$cst)))
 colnames(weather_data) = tolower(gsub('\\.',"_",colnames(weather_data)))
+weather_data$FL_DATE = format(as.Date(as.character(weather_data$cst)))
+
 
 write.csv(weather_data,file = '../data/final/final_weather_data.csv',
           row.names = F)
--- codes/02-final-dataset-creation.R
+++ codes/02-final-dataset-creation.R
@@ -4,23 +4,47 @@ setwd("C:/Users/Anirudh Narayanan/Desktop/Some stuff/msba stuff/Summer/Stats/msb
 
 dataloc = '../data/final/'
 delays = read.csv(paste0(dataloc,'final_delay_data.csv'),stringsAsFactors = F)
-delays$date = format(as.Date(delays$FL_DATE))
+
 
 weather = read.csv(paste0(dataloc,'final_weather_data.csv'),stringsAsFactors = F)
+colnames(weather)[colnames(weather) == 'FLDATE'] <- 'FL_DATE'
 colnames(weather)[colnames(weather) == 'location'] <- 'ORIGIN'
-weather$date = format(as.Date(weather$cst))
-
-final = merge(x = delays, y = weather, by = c('date','ORIGIN'), all.x=T)
-
-model_data = final[which(final$DEP_DELAY >= 10 & final$ORIGIN == 'DFW' & ),]
-model_data$DEP_DELAY[which(model_data$DEP_DELAY>= 377)] = 377
-model_data$has_events = nchar(model_data$events)
-model_data$has_events[which(model_data$has_events > 0)] = 1
 
 
-summary(model_data$DEP_DELAY)
-quantile(model_data$DEP_DELAY,seq(0.9,1,0.005))
+final = merge(x = delays, y = weather, by = c('FL_DATE','ORIGIN'), all.x=T)
 
 
-summary(lm(DEP_DELAY ~ mean_visibilitymiles + max_temperaturef + min_visibilitymiles + winddirdegrees
-           + has_events, data = model_data))
+model_data = final[which(final$DEP_DELAY >= 10 & final$ORIGIN == 'MSP' ),]
+model_data = final[which(final$ORIGIN == 'MSP'),]
+model_data$delay_treated = ifelse(model_data$DEP_DELAY>= 330,330,model_data$DEP_DELAY)
+model_data$ln_delay = log(model_data$DEP_DELAY + 1)
+
+##HAS EVENTS?
+model_data$has_events = ifelse(nchar(model_data$events)>0, 1,0)
+
+##MAJOR/MINOR AIRLINES
+model_data$airline_class = ifelse(model_data$AIRLINE_ID %in% c(19805,19790,19393,19977,19531,19930,
+                                                          20229,21294,20416,20435,21351,19690,
+                                                          20368,21171,21578,21226,19961,21342,
+                                                          20422),
+                                  1,0)
+
+
+##IS WEEKEND?
+model_data$is_weekend = ifelse(model_data$DAY_OF_WEEK > 4, 1,0)
+
+##HOUR OF DEPARTURE
+model_data$dep_hr = model_data$CRS_DEP_TIME%/%100
+
+summary(lm(delay_treated ~
+            min_visibilitymiles +
+            min_temperaturef +
+            min_visibilitymiles +
+            winddirdegrees +
+            as.factor(events) +
+            airline_class +
+            QUARTER +
+            AIR_TIME +
+            dep_hr
+            ,
+           data = model_data))
